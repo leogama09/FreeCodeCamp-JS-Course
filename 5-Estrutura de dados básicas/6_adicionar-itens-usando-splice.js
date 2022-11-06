995588e24cbd99018329d53063d2315b74301068
Leonardo Gama <leogama09@gmail.com>
2022-11-06 20:37:19 -0300

    Update 6_adicionar-itens-usando-splice.js

--- 5-Estrutura de dados básicas/6_adicionar-itens-usando-splice.js
+++ 5-Estrutura de dados básicas/6_adicionar-itens-usando-splice.js
@@ -1,6 +1,9 @@
 function htmlColorNames(arr) {
   // Altere apenas o código abaixo desta linha
-arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+  let startIndex = 0
+  let amoutToDelete = 2
+  
+  arr.splice(startIndex, amoutToDelete, 'DarkSalmon', 'BlanchedAlmond')
   // Altere apenas o código acima desta linha
   return arr;
 }
