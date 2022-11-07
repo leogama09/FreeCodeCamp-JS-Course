bf4eafab2c6c2c6ae7db80ce843776fcc462f738
Leonardo Gama <leogama09@gmail.com>
2022-11-07 15:33:24 -0300

    Exercícios React 3-10

---
+++ React/7-criar-um-componente-funcional-sem-estado.jsx
@@ -0,0 +1,9 @@
+const MyComponent = function() {
+  // Altere o código abaixo desta linha
+  return (
+    <div>'String de texto'</div>
+  )
+
+
+  // Altere o código acima desta linha
+}
---
+++ React/8-criar-um-componente-do-react.jsx
@@ -0,0 +1,14 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Altere o código abaixo desta linha
+return (
+<div>
+<h1>Hello React!</h1>
+</div>
+)
+    // Altere o código acima desta linha
+  }
+};
