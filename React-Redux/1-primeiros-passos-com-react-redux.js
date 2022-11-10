ef065b1e100d2e3a23537cb1d1bf4c7bf533b6de
Leonardo Gama <leogama09@gmail.com>
2022-11-10 16:39:21 -0300

    Create 1-primeiros-passos-com-react-redux.js

---
+++ React-Redux/1-primeiros-passos-com-react-redux.js
@@ -0,0 +1,15 @@
+class DisplayMessages extends React.Component {
+  // Altere o código abaixo desta linha
+  constructor(props) {
+    super(props)
+    this.state = {
+    input:'',
+    messages: []
+    }
+
+  }
+  // Altere o código acima desta linha
+  render() {
+    return <div />
+  }
+};
