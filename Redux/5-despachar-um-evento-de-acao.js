dba2b8317deefa8f93e00133bb38d8772e1c23df
Leonardo Gama <leogama09@gmail.com>
2022-11-10 14:08:00 -0300

    Create 5-despachar-um-evento-de-acao.js

---
+++ Redux/5-despachar-um-evento-de-acao.js
@@ -0,0 +1,12 @@
+const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Despache a ação aqui:
+store.dispatch(loginAction())
